d1d8d58935fd867992b0b2dc8b7b9c4b7dcdac59
jbusecke <julius@ldeo.columbia.edu>
2017-03-20 12:01:59 -0400

    why was this not pusshed

--- mitgcm_surface_tracer/tracer_processing.py
+++ mitgcm_surface_tracer/tracer_processing.py
@@ -10,7 +10,8 @@ import xgcm
 from dask.diagnostics import ProgressBar
 from xmitgcm import open_mdsdataset
 from xarrayutils.utils import aggregate,aggregate_w_nanmean
-from xarrayutils.xmitgcm_utils import matching_coords,laplacian,gradient_sq_amplitude
+from xarrayutils.xmitgcm_utils import gradient_sq_amplitude,matching_coords
+from xarrayutils.xmitgcm_utils import laplacian
 from xarrayutils.build_grids import grid_aggregate
 from .utils import readbin, paramReadout, dirCheck
 
