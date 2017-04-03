bfe836fe18939e7b8fe47be1b19f4fbf7cf2073c
jbusecke <julius@ldeo.columbia.edu>
2017-04-03 15:17:41 -0400

    corrected validmask

--- mitgcm_surface_tracer/velocity_processing.py
+++ mitgcm_surface_tracer/velocity_processing.py
@@ -1,64 +1,68 @@
 from __future__ import print_function
-from future.utils import iteritems
 import xarray as xr
 import numpy as np
 import time
 import os
 import os.path
 import matplotlib as mpl
-mpl.use('Agg')
-import matplotlib.pyplot as plt
 from xmitgcm import open_mdsdataset
 from xarrayutils.numpy_utils import interp_map_regular_grid
 from .utils import readbin, writebin
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 
-def validmask_aviso(da_u,da_v):
-    validmask_aviso_u = np.any(np.isnan(da_u),axis = da_u.get_axis_num('time'))
-    validmask_aviso_v = np.any(np.isnan(da_v),axis = da_v.get_axis_num('time'))
 
-    if np.logical_xor(validmask_aviso_u,validmask_aviso_v).any():
+def validmask_aviso(da_u, da_v):
+    validmask_aviso_u = np.all(~np.isnan(da_u),
+                               axis=da_u.get_axis_num('time'))
+    validmask_aviso_v = np.all(~np.isnan(da_v),
+                               axis=da_v.get_axis_num('time'))
+
+    if np.logical_xor(validmask_aviso_u, validmask_aviso_v).any():
         raise RuntimeWarning('U and V validmask are not equal')
 
-    validmask_aviso = np.logical_or(validmask_aviso_u,validmask_aviso_v)
+    validmask_aviso = np.logical_and(validmask_aviso_u, validmask_aviso_v)
     return validmask_aviso
 
-def combine_validmask(data_dir,shape=None,debug=False):
+
+def combine_validmask(data_dir, shape=None, debug=False):
 
     fnames = []
     for dirpath, dirnames, filenames in os.walk(data_dir):
-        for filename in [f for f in filenames if f=='validmask.bin']:
+        for filename in [f for f in filenames if f == 'validmask.bin']:
             print('found validmask at '+os.path.join(dirpath, filename))
             fnames.append(os.path.join(dirpath, filename))
     if debug:
-        print('data_dir',data_dir)
+        print('data_dir', data_dir)
         print(fnames)
 
     if shape:
-        masks = np.array([readbin(f,shape) for f in fnames])
+        masks = np.array([readbin(f, shape) for f in fnames])
     else:
         raise RuntimeWarning('When shape is not given')
 
-
-    combo = np.any(np.stack(masks,axis=2),axis=2)
+    combo = np.all(np.stack(masks, axis=2), axis=2)
 
     fpath = data_dir+'/validmask_combined.bin'
-    writebin(combo,fpath)
+    writebin(combo, fpath)
     print('--- combined validmask written to '+fpath+' ---')
 
-def process_aviso(odir,gdir,ddir_dt,fid_dt,ddir_nrt=None,fid_nrt=None,interpolate=True):
+
+def process_aviso(odir, gdir, ddir_dt, fid_dt,
+                  ddir_nrt=None, fid_nrt=None, interpolate=True):
     """read aviso files into xarray dataset, respecting 'seam' between
     delayed-time
     product and near-real time products
 
     """
-    grid = open_mdsdataset(gdir,iters=None)
-    ds_dt = xr.open_mfdataset(ddir_dt+'/'+fid_dt).drop(['nv','crs'])
-    if not ddir_nrt==None:
+    grid = open_mdsdataset(gdir, iters=None)
+    ds_dt = xr.open_mfdataset(ddir_dt+'/'+fid_dt).drop(['nv', 'crs'])
+    if ddir_nrt is not None:
         transition_date = ds_dt.time.isel(time=-1)
-        ds_nrt = xr.open_mfdataset(ddir_nrt+'/'+fid_nrt).drop(['nv','crs'])
+        ds_nrt = xr.open_mfdataset(ddir_nrt+'/'+fid_nrt).drop(['nv', 'crs'])
         ds = xr.concat((ds_dt,
-                        ds_nrt.isel(time=ds_nrt.time>transition_date)),
-                        dim='time')
+                        ds_nrt.isel(time=ds_nrt.time > transition_date)),
+                       dim='time')
         # Test if time is continous
         if np.any(ds.time.diff('time').data != ds.time.diff('time')[0].data):
             print(ds.time)
@@ -83,16 +87,19 @@ def process_aviso(odir,gdir,ddir_dt,fid_dt,ddir_nrt=None,fid_nrt=None,interpolat
     # create and save validmask
     # validmask indicates values that were interpolated or filled
     # and should be taken out for certain interpretations.
-    validmask = validmask_aviso(ds.u,ds.v)
-    validmask_mit = interp_map_regular_grid(validmask,ds.u.lon.data,ds.u.lat.data,grid.XC.data,grid.YC.data)
+    validmask = validmask_aviso(ds.u, ds.v)
+    validmask_mit = interp_map_regular_grid(validmask,
+                                            ds.u.lon.data,
+                                            ds.u.lat.data,
+                                            grid.XC.data,
+                                            grid.YC.data)
     # fill the values between 0 and with 1 to exclude them from valid points.
-    validmask_mit[validmask_mit!=0] = 1
-    writebin(validmask_mit,odir+'/validmask.bin')
+    validmask_mit[validmask_mit != 0] = 1
+    writebin(validmask_mit, odir+'/validmask.bin')
     print('--- validmask written to '+odir+'/validmask.bin ---')
 
     start_time = time.time()
-    for tt,ti in enumerate(ds.time.data):
-    # for tt in [0]:
+    for tt, ti in enumerate(ds.time.data):
         u = ds.u.sel(time=ti).values
         v = ds.v.sel(time=ti).values
 
@@ -100,20 +107,29 @@ def process_aviso(odir,gdir,ddir_dt,fid_dt,ddir_nrt=None,fid_nrt=None,interpolat
         u[np.isnan(u)] = 0
         v[np.isnan(v)] = 0
 
-        u_int = interp_map_regular_grid(u,ds.u.lon.data,ds.u.lat.data,grid.XG.data,grid.YC.data)
-        v_int = interp_map_regular_grid(v,ds.v.lon.data,ds.v.lat.data,grid.XC.data,grid.YG.data)
+        u_int = interp_map_regular_grid(u,
+                                        ds.u.lon.data,
+                                        ds.u.lat.data,
+                                        grid.XG.data,
+                                        grid.YC.data)
+        v_int = interp_map_regular_grid(v,
+                                        ds.v.lon.data,
+                                        ds.v.lat.data,
+                                        grid.XC.data,
+                                        grid.YG.data)
 
         if np.any(np.isnan(u_int.flatten())):
             raise RuntimeError('Nans detected in the u fields')
         if np.any(np.isnan(v_int.flatten())):
             raise RuntimeError('Nans detected in the v fields')
 
-        writebin(u_int,odir+'/uvel'+str(ti))
-        writebin(v_int,odir+'/vvel'+str(ti))
+        writebin(u_int, odir+'/uvel'+str(ti))
+        writebin(v_int, odir+'/vvel'+str(ti))
 
         print(str(tt)+'/'+str(len(ds.time)))
         print(str(ti))
 
-    print("--- Velocity Interpolation took %s seconds ---" % (time.time() - start_time))
+    print("--- Velocity Interpolation took %s seconds ---"
+          % (time.time() - start_time))
 
     return ds
