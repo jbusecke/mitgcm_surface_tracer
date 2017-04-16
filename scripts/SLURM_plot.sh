3333064a0d4855db4d42e5a967ab5616390ddacc
jbusecke <julius@ldeo.columbia.edu>
2017-04-16 09:45:28 -0400

    minor changes to SLURM-Plot AGAIN and AGAIN

--- scripts/SLURM_plot.sh
+++ scripts/SLURM_plot.sh
@@ -8,29 +8,17 @@
 #SBATCH --mail-user=julius@ldeo.columbia.edu
 #SBATCH --mail-type=ALL
 
-if [ "$1" == "dev" ]
-then
-  echo "DEV MODE ACTIVE"
-  RUNDIR="/Volumes/EXTERNAL_WORK/run_KOC_daily_LAT"
-  # PYTHONDIR="/Users/juliusbusecke/Work/PROJECTS/COLL_RYAN/ROUTINES/SETUP"
-  # PBSDIR=$PYTHONDIR
-  VALIDPATH="/Users/juliusbusecke/Work/PROJECTS/COLL_RYAN/OUTPUT/AVISO_validmask/validmask_combined.bin"
-else
-  RUNDIR=$PBS_O_WORKDIR
-  # PYTHONDIR=/vega/physo/users/jb3210/tracer_model_scripts
-  # PBSDIR=$RUNDIR
-  VALIDPATH="/vega/physo/users/jb3210/offline_velocities/aviso_DUACS2014_daily_msla/interpolated/validmask_combined.bin"
-  source activate standard
-fi
-
-
+RUNDIR=$SLURM_SUBMIT_DIR
+VALIDPATH="/rigel/ocp/users/jb3210/aviso_surface_tracer/offline_velocities/aviso_DUACS2014_daily_msla/interpolated/validmask_combined.bin"
 PDIR="$RUNDIR/plots"
-rm -r plots
 
+rm -r plots
 # remake the directory
 mkdir $PDIR
 
+
 echo 'START PYTHON QC PLOTS'
+source activate standard
 python -c 'from mitgcm_surface_tracer.tracer_visualization import main;\
 main("'$RUNDIR/'","'$PDIR/'");\
 exit();'
