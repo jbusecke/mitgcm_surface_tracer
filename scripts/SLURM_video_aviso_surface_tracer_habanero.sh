fbeef59adbcfa75078e2fc153075a42a0ab3a509
Julius Busecke <jb3210@columbia.edu>
2017-04-27 11:51:39 -0400

    Improvement/nature paper (#14)
    * added script, refactored processing
    
    * changed permissions
    
    * More time for the full run with tracer var budget
    
    * increased run length on habanero
    
    * increased run length to max on habanero
    
    * raw output disabled by default
    
    * raw output disabled by default, ammended
    
    * tuned time of Habanero scripts
    
    * forgot to save some scripts

--- scripts/SLURM_video_aviso_surface_tracer_habanero.sh
+++ scripts/SLURM_video_aviso_surface_tracer_habanero.sh
@@ -2,13 +2,14 @@
 
 #SBATCH --account=ocp
 #SBATCH --exclusive
-#SBATCH -N 2
+#SBATCH -N 4
 #SBATCH -J aviso_video
-#SBATCH --time=8:00:00
+#SBATCH --time=1:00:00
 #SBATCH --mail-user=julius@ldeo.columbia.edu
 #SBATCH --mail-type=ALL
 
 echo $SLURM_SUBMIT_DIR
 cd $SLURM_SUBMIT_DIR
 source activate standard
-python -c 'from xarrayutils.visualization import mitgcm_Movie; mitgcm_Movie("'$SLURM_SUBMIT_DIR'"); exit();'
+python -c 'from xarrayutils.visualization import mitgcm_Movie; \
+  mitgcm_Movie("'$SLURM_SUBMIT_DIR'"); exit();'
